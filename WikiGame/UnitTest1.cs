610d69c4651c5516239ca195d354792a4f2ef55f
Sven Bächler <sven.baechler@stud.hslu.ch>
2023-10-20 15:46:06 +0200

    initial commit

---
+++ WikiGame/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WikiGame
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
